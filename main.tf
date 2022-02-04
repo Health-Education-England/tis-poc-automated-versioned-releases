26273363d3772e08832b1403dd13305af3dc5ef2
Andy Dingley <Judge40@users.noreply.github.com>
2022-02-04 18:25:48 +0000

    chore: add missing main.tf file
    TIS21-SHED

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.38"
+    }
+  }
+}
+
+locals = {
+  tags = {
+    ModuleVersion = "0.0.1"
+  }
+}
+
+data "dummy" "dummy1" {
+  dummy = dummy1
+}
+
+data "dummy" "dummy2" {
+  dummy = dummy2
+}
