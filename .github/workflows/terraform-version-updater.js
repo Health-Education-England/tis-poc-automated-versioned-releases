cee1315bdc378ddfe82227188383adae2881c469
Andy Dingley <Judge40@users.noreply.github.com>
2023-11-10 18:45:29 +0000

    feat: use remote workflow

--- .github/workflows/terraform-version-updater.js
+++
@@ -1,9 +0,0 @@
-const moduleVersionRegex = /(?<prefix>\sModuleVersion = ")(?<version>\d+.\d+.\d+)(?<suffix>"\s)/
-
-module.exports.readVersion = function (contents) {
-  return contents.match(moduleVersionRegex).groups.version;
-}
-
-module.exports.writeVersion = function (contents, version) {
-  return contents.replace(moduleVersionRegex, `$<prefix>${version}$<suffix>`)
-}
